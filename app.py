a3115b644a96071afecc5df4a2f27dbbadfc98e9
Cyrus Bhandari <cbhandari@paloaltonetworks.com>
2024-02-15 16:42:11 +0530

    flask app updated

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+import jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
     return "THE HOMEPAGE"
+
+@app.route("/api/about")
+def about_list():
+    return jsonify()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
